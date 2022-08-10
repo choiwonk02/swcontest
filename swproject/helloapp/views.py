8217814869217124c14a5feb9172ffff509ba13e
choiwonk02 <wongyoung80@gmail.com>
2022-08-11 00:09:40 +0900

    second
    댓글창(넘어가는 페이지), 홈 하이퍼링크 구현

--- swproject/helloapp/views.py
+++ swproject/helloapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Search
 
-# Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def service(request):
+    return render(request, 'service.html')
+
+def search(request):
+    if request.method=='POST':
+        searched = request.POST['searched']        
+        contents = Search.objects.filter(name__contains=searched)   #contents=혼잡도
+        return render(request, 'searched.html', {'searched': searched, 'contents': contents})
+    else:
+        return render(request, 'searched.html', {})
